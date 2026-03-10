a423562c35f90eafc7148ea5d6b62529d1359dee
ring1012 <1223305593@qq.com>
2026-03-10 22:36:11 +0800

    kv test

--- edge-functions/hello-edge.js
+++ edge-functions/hello-edge.js
@@ -1,6 +1,6 @@
-export default async function onRequest(context) {
+export default async function onRequestGet({ request, params, env }) {
 
-  const testData = await r1.get("test");
+  const testData = await r1_kv.get("test");
 
   return new Response(JSON.stringify({
     message: testData,
